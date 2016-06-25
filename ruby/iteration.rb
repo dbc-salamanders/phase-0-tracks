557673ca8560a95ab5ed4bda615b39a02c9db0db
Aaron Larkin <aclarkin1@gmail.com>
2016-06-25 11:51:15 +0000

    Iterate through hash and array. Modify array using .map!

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,4 +1,24 @@
+## Add array and hash
+
 tennis_players = ["Federer", "Djokovic", "McEnroe", "Lendl"]
 federer = {:forehand => "eastern", :backhand => "one-handed", :majors => 17, :serve_volley => true}
 
-puts federer
+## Iterate through each item in array
+
+tennis_players.each do |legend|
+  puts "#{legend} is a tennis legend."
+end
+
+## Iterate through eah item in hash
+
+federer.each do |key, value|
+  puts "Roger Federer's #{key} is #{value}."
+end
+
+##Modify the items in the array using .map!
+
+tennis_players.map! do |legend|
+  legend.upcase
+end
+
+puts tennis_players
