7ae03ea842d335aa2a6cd899c8fb8c8017b07ab5
Aaron Larkin <aclarkin1@gmail.com>
2016-06-26 14:02:21 +0000

    Add method to filter data structures for items meeting condition

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,6 +1,6 @@
 ## Add array and hash
 
-tennis_players = ["Federer", "Djokovic", "McEnroe", "Lendl"]
+tennis_players = ["Federer", "Djokovic", "McEnroe", "Lendl", "Federer"]
 federer = {:forehand => "eastern", :backhand => "one-handed", :majors => 17, :serve_volley => true}
 
 ## Iterate through each item in array
@@ -21,4 +21,22 @@ tennis_players.map! do |legend|
   legend.upcase
 end
 
-puts tennis_players
+## A method that iterates through the items, deleting any that meet a certain condition
+
+tennis_players.delete_if { |legend| legend.length < 6}
+
+federer.delete_if {|key, value| key == :forehand}
+
+##A method that filters a data structure for only items that do satisfy a certain condition
+
+tennis_players.keep_if { |legend| legend.length > 7}
+
+federer.keep_if {|key, value| value == 17}
+
+##A different method that filters a data structure for only items satisfying a certain condition
+
+puts tennis_players.uniq
+
+federer.each_value {|value| puts value}
+
+
