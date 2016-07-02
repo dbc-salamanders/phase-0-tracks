dd65fd50da66b04487490dd727e6f288bd99ea73
Aaron Larkin <aclarkin1@gmail.com>
2016-07-02 15:31:01 +0000

    Add roll_over and dog_years methods

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -14,8 +14,21 @@ class Puppy
   	times
   end
 
+  def roll_over
+  	puts "*rolls over*"
+  end
+  
+ def dog_years(human_years)
+ 	dog_age = 10.5 + (human_years - 2) * 4
+ 	puts dog_age
+ 	dog_age
+ end
+
 end
 
 mochi = Puppy.new
 mochi.fetch("tennis ball")
 mochi.speak(7)
+mochi.roll_over
+mochi.dog_years(5)
+
