795a7af470b1c592bb89b1fd7cd6ad5081584d9c
Aaron Larkin <aclarkin1@gmail.com>
2016-07-03 00:44:57 +0000

    Add loop, new instance in each loop. Store each instance in hash. Iterate over hash by calling methods on each instance

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -37,9 +37,70 @@ end
 
 end
 
-mochi = Puppy.new
+#mochi = Puppy.new
 #mochi.fetch("tennis ball")
 #mochi.speak(7)
 #mochi.roll_over
 #mochi.dog_years(5)
 #mochi.doggy_treats
+
+class Wimbledon
+	
+	def initialize
+#		puts "Wimbledon 2016..."
+	end
+	
+	def countdown(times)
+	  puts "Play was suspended due to rain"
+	  puts "----"
+	  puts "Play will resume in..."
+	  i = times
+	  until i == 0
+	  puts "#{i}..."
+	  i -= 1
+	  end
+	  times
+	end
+	
+	def match
+		players = ["Federer", "Murray", "Del Potro", "Gasquet", "Thiem", "Djokovic", "Isner", "Ferrer", "Nishikori", "Wawrinka"]
+		other_players = ["Berdych", "Coric", "Ram", "Harrison", "Raonic", "Cilic", "Haas", "Mayer", "Fritz", "Stepanek"]
+		puts "#{players.sample} will play #{other_players.sample} in today's match"
+	end
+	
+	def new_balls
+	  puts "New balls, please!"
+	end
+	
+end
+
+#wimby = Wimbledon.new
+#wimby.semi_finals
+#wimby.countdown(5)
+
+# Initiate a loop to create 50 instances of Wimbledon class
+# Until the loop has repeated 50 times, create a new instance of Wimbledon class
+# Store each new instance in a hash
+# Make the key for the hash the index, and the value the instance of Wimbledon class
+
+wimby_hash = {}
+index = 1
+until index == 51
+	new_instance = Wimbledon.new
+	wimby_hash[index] = new_instance
+  index += 1
+end
+
+#p wimby_hash
+
+# Iterate over the hash created above
+# Call the method match method for each instance
+
+wimby_hash.each do |key, instance|
+  puts "Wimbledon match # #{key}..."
+  instance.match
+  instance.new_balls
+  instance.countdown(5)
+end
+
+#p wimby_hash
