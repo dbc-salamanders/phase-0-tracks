c6d65ef9962a788a6adfd08e6b283c447ab2d865
Aaron Larkin <aclarkin1@gmail.com>
2016-07-03 16:34:07 +0000

    Pseudocode class design

---
+++ ruby/solo.rb
@@ -0,0 +1,12 @@
+### Release 0 - design a class
+# Class will be a soccer player (class Soccer_Player)
+# Attributes will be age(integer), position(string), club(string), up_for_transfer(boolean)
+# Initialize attributes above as instance variables in initialize method
+# First method will be change_position. Method reassigns the players position
+  # Note: make position readable and writable
+# Second method will evaluate if player is up for transfer 
+  # If up_for_transfer = true, print that player is considering transfer to a randomly selected team
+  # If up_for_transfer = false, print that player is happy at @club
+# Third method will track how many kilometers player ran in last match
+  # Method takes kilometers as an argument(integer)
+  
