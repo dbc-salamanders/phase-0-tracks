9bfab789750ce6b6b742513d217af972e8d73f79
Aaron Larkin <aclarkin1@gmail.com>
2016-07-03 18:36:51 +0000

    Clean up interface to allow for optionality. Switch data structure from array to hash. Iterate over hash

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -35,7 +35,7 @@ attr_accessor :position, :up_for_transfer, :age, :club
   end
   
   def kilometers_ran(kilometers)
-    puts "This player ran #{kilometers} kilometers in his last match"
+    puts "This player can put in a shift. In fact, he ran #{kilometers} kilometers in his last match."
     return kilometers
   end
   
@@ -55,7 +55,7 @@ p sadio
 =end
 
 ### Release 2
-# Initiate an empty array to store data in
+# Initiate an empty hash to store data in
 # Initiate a loop. done = false. Until done = true, loop the below
 # Initiate a new instance of Fussballer class
 # Prompt the user to create a soccer player by entering his/her age. input = @age
@@ -67,13 +67,13 @@ p sadio
 # Prompt the user to enter (as a number) how many kilometers player ran in the last match. Input = kilometers
 # Using the variables collected, print a statement detailing the players age, position, and club
 # Run the transfer_gossip and kilometers_ran methods
-# Push the instance of the class created into the array
+# Push the instance of the class created into the hash as the key
+# Push kilometers into the hash as the value
 # Ask the user to type "Done" if they are finished, or any other key to continue
   # If input = "Done", then done = true and loop ends
   # Else, repeat loop
 
-players = []
-p players
+players = {}
 done = false
 until done == true do
   player = Fussballer.new(@age, @position, @club)
@@ -83,30 +83,38 @@ until done == true do
     player.position = gets.chomp.to_s
   puts "Is the player interested in switching position?"
     position_input = gets.chomp.to_s
-    if position_input == "Yes"
+    if position_input = ["Yes", "yes", "Yes ", " Yes", " yes", "y", "Y", " y", " Y", "y ", "Y "].include?(position_input)
       player.change_position
     end
   puts "Which club does the player play for?"
     player.club = gets.chomp
   puts "Is the player interested in a transfer to a different club?"
     transfer_input = gets.chomp.to_s
-    if transfer_input == "Yes"
-      player.up_for_transfer == true
+    if transfer_input = ["Yes", "yes", "Yes ", " Yes", " yes", "y", "Y", " y", " Y", "y ", "Y "].include?(transfer_input)
+      player.up_for_transfer = true
     else
       player.up_for_transfer = false
     end
   puts "How many kilometers did the player run in the last match?"
     kilometers = gets.chomp.to_i
-  puts "Ok, great. So you have created a player who is #{player.age} years old, plays in the #{player.position} position for #{player.club}."
-  player.transfer_gossip
-  players << player
+  players[player] = kilometers
   puts "Type 'done' to exit, or any other key to create another player"
     repeat = gets.chomp.to_s
-    if repeat == "Done"
+    if repeat = ["Done", "done", " Done", " done", "Done ", "done "].include?(repeat)
       done = true
     else
       done = false
     end
 end
 
-puts players
+#puts players
+
+# Iterate over the players hash to call methods defined above
+# Print a message for each instance in the hash to confirm that the data was collected/processed properly
+
+players.each do |instance, distance|
+  puts "Ok, great. So you have created a player who is #{instance.age} years old, plays in the #{instance.position} position for #{instance.club}."
+  instance.transfer_gossip
+  instance.kilometers_ran(distance)
+  puts "----"
+end
