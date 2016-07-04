a2bb6ba833b89717e6890906f66b15b353a0e1f0
Aaron Larkin <aclarkin1@gmail.com>
2016-07-04 15:18:40 +0000

    Add yell_angrily and yelling_happily methods to Shout module

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,5 +1,15 @@
 module Shout
 
-
+  def self.yell_angrily(words)
+    words + "!!!" + " :("
+  end
+  
+  def self.yelling_happily(words_of_praise)
+    words_of_praise + " Hallelujah!" + " =D"
+  end
 
 end
+
+#Driver code to methods
+p Shout.yell_angrily("Oh no")
+p Shout.yelling_happily("It's a long weekend!")
