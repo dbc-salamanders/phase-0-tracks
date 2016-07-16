6390cf7f0a4b93034a034421d9ebbeb74931ba38
Aaron Larkin <aclarkin1@gmail.com>
2016-07-16 13:53:56 +0000

    Add add_name and add_color functions

---
+++ js/data_structures.js
@@ -0,0 +1,16 @@
+var colors = ["red", "white", "blue", "purple"];
+var names = ["Ed", "James", "Lisa", "Sarah"]
+
+function add_color(color) {
+  colors.push(color)
+}
+
+function add_name(name) {
+	names.push(name)
+}
+
+add_color("brown")
+console.log(colors)
+
+add_name("Ray")
+console.log(names)
