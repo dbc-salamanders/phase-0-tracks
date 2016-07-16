5e28f59a449461e6e18431425d49c4049b326e8d
Aaron Larkin <aclarkin1@gmail.com>
2016-07-16 14:39:45 +0000

    Add horse_object function

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,5 +1,5 @@
-var colors = ["red", "white", "blue", "purple"];
 var names = ["Ed", "James", "Lisa", "Sarah"]
+var colors = ["red", "white", "blue", "purple"];
 
 function add_color(color) {
   colors.push(color)
@@ -14,3 +14,21 @@ console.log(colors)
 
 add_name("Ray")
 console.log(names)
+
+// Release 1 - build an object
+// Declare a function horse_object
+// Inside horse_object, declare an empty object. Store as variable 'object'
+// The function will loop through each item in the names array, starting at index 0
+// The item in names corresponding to the current index will be pushed into object
+// Add a colon in object to each item name pushed in - to designate as key
+// Add to object the item from the colors index that corresponds to the same index
+
+function horse_object() {
+	var object = {};
+  	for (var i = 0; i < names.length; i++) {
+	object[names[i]] = colors[i];
+ }
+ console.log(object);
+}
+
+horse_object();
